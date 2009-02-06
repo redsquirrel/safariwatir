2ce1c9722b9855fc1e94e8a1ddde1f79a3327a72
Dave Hoover <redsquirrel@HooverObtiva2.local>
2009-02-05 21:26:02 -0600

    Trying to get this working on GitHub

---
+++ safariwatir/exceptions.rb
@@ -0,0 +1,4 @@
+class MissingWayOfFindingObjectException < Exception; end
+class UnknownObjectException < Exception; end
+class UnknownCellException < Exception; end
+class UnknownFrameException; end
