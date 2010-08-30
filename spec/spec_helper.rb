aef6d678cc45cfcf5d1e6ac4eef8e10acf877837
TreyE <levans@eoriginal.com>
2010-08-31 02:10:30 +0800

    Adding the #host method back to WatirSpec.
    I got tired of seeing tests that couldn't pass because of the missing method.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib/')
 require "safariwatir"
+require "sinatra/base"
 
 Browser = Watir::Safari
 include Watir::Exception
+
+module WatirSpec
+
+  class Server < Sinatra::Base
+    def self.host
+      "127.0.0.1"
+    end
+  end
+
+end
